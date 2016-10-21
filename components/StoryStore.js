43bd81a99a3c89f1731b7129d4846a39fd2784a6
anna <annjellyiu5@gmail.com>
2016-10-21 22:22:23 +0300

    search bar

--- components/StoryStore.js
+++ components/StoryStore.js
@@ -12,7 +12,7 @@ import {
     Picker
     } from 'react-native';
 import Loader from './Loader.js'
-
+import { Spinner, Content, Container, Header, Title, Icon, InputGroup, Input, ListItem, List, Radio, CheckBox, Thumbnail, Card, CardItem, H3 } from 'native-base';
 import Button from "react-native-button";
 var movieReviewsFromApi = [
     {name: 'Die Hard'}, {name: 'Home Alone 2'}, {name: 'Bourne Identity'}, {name: 'Die Hard'}, {name: 'Home Alone 2'}, {name: 'Bourne Identity'}, {name: 'Die Hard'}, {name: 'Home Alone 2'}, {name: 'Bourne Identity'},  {name: 'Die Hard'}, {name: 'Home Alone 2'}, {name: 'Bourne Identity'},  {name: 'Die Hard'}, {name: 'Home Alone 2'}, {name: 'Bourne Identity'}
@@ -36,7 +36,8 @@ export default class StoryStore extends Component{
             cat: 'all',
             page: 0,
             allStories: false,
-            ordtype: 'desc'
+            ordtype: 'desc',
+            search: ''
 
         };
     }
@@ -74,8 +75,6 @@ export default class StoryStore extends Component{
                 <Text style={styles.text}>Длительность: {rowData.minutes}:{rowData.seconds}</Text>
                 <Text style={styles.text}>Цена: {rowData.price} руб.</Text>
                 <Button
-                    containerStyle={styles.buttonStyle}
-                    style={styles.textStyle}
                     onPress={this.onBuy.bind(this, rowData.id)}
                    >
                     Купить
@@ -89,74 +88,118 @@ export default class StoryStore extends Component{
     }
     render() {
         return (
-            <View style={styles.listParent}>
-                <View>
-                    <Text>Категория:</Text>
-                    <Picker
-                        style={styles.picker}
-                        selectedValue={this.state.cat}
-                        onValueChange={this.onValueChange.bind(this, 'cat')}
-                        mode="dialog"
-                        prompt="Категория:">
-                        { categories.map((s, i) => {
-                            return <Item
-                                        key={i}
-                                        value={s.value}
-                                        label={s.label} />
+        <Container style={styles.listParent}>
+            <Header searchBar rounded>
+                <InputGroup>
+                    <Icon name="ios-search" />
+                    <Input placeholder="Search" value={this.state.search}  onChangeText={(text) => this.setState({search:text})} onSubmitEditing={()=>this.search()}/>
+                </InputGroup>
+                <Button transparent onPress={()=>this.search()}>Go</Button>
+            </Header>
+                <View style={styles.listParent}>
+                    <View>
+                        <Text>Категория:</Text>
+                        <Picker
+                            style={styles.picker}
+                            selectedValue={this.state.cat}
+                            onValueChange={this.onValueChange.bind(this, 'cat')}
+                            mode="dialog"
+                            prompt="Категория:">
+                            { categories.map((s, i) => {
+                                return <Item
+                                    key={i}
+                                    value={s.value}
+                                    label={s.label} />
                             }) }
-                    </Picker>
-                    <Text>Упорядочить по:</Text>
-                    <Picker
-                        style={styles.picker}
-                        selectedValue={this.state.order}
-                        onValueChange={this.onValueChange.bind(this, 'order')}
-                        mode="dialog"
-                        prompt="Упорядочить по:">
-                        { orderField.map((s, i) => {
-                            return <Item
-                                key={i}
-                                value={s.value}
-                                label={s.label} />
-                        }) }
-                    </Picker>
-                    <Text>Порядок сортировки:</Text>
-                    <Picker
-                        style={styles.picker}
-                        selectedValue={this.state.ordtype}
-                        onValueChange={this.onValueChange.bind(this, 'ordtype')}
-                        prompt="Порядок сортировки:">
-                        { orderTypes.map((s, i) => {
-                            return <Item
-                                key={i}
-                                value={s.value}
-                                label={s.label} />
+                        </Picker>
+                        <Text>Упорядочить по:</Text>
+                        <Picker
+                            style={styles.picker}
+                            selectedValue={this.state.order}
+                            onValueChange={this.onValueChange.bind(this, 'order')}
+                            mode="dialog"
+                            prompt="Упорядочить по:">
+                            { orderField.map((s, i) => {
+                                return <Item
+                                    key={i}
+                                    value={s.value}
+                                    label={s.label} />
                             }) }
-                    </Picker>
+                        </Picker>
+                        <Text>Порядок сортировки:</Text>
+                        <Picker
+                            style={styles.picker}
+                            selectedValue={this.state.ordtype}
+                            onValueChange={this.onValueChange.bind(this, 'ordtype')}
+                            prompt="Порядок сортировки:">
+                            { orderTypes.map((s, i) => {
+                                return <Item
+                                    key={i}
+                                    value={s.value}
+                                    label={s.label} />
+                            }) }
+                        </Picker>
+                    </View>
+                    <Button
+                        onPress={this._onRefresh.bind(this,false)}
+                        >
+                        Найти
+                    </Button >
+                    <View style={[styles.listParent, styles.container]}>
+                        <ListView
+                            dataSource={this.state.dataSource}
+                            renderRow={(rowData) => this._renderRow(rowData)}
+                            renderScrollComponent={props => <RecyclerViewBackedScrollView {...props} />}
+                            renderSeparator={StoryStore._renderSeparator}
+                            renderFooter={this.renderFooter.bind(this)}
+                            enableEmptySections={true}
+                            onEndReachedThreshold={10}
+                            scrollEventThrottle={10}
+                            />
+                    </View>
                 </View>
-                <Button
-                    containerStyle={styles.buttonStyle7}
-                    style={styles.textStyle6}
-                    onPress={this._onRefresh.bind(this,false)}
-                    >
-                    Найти
-                </Button >
-                <View style={[styles.listParent, styles.container]}>
-                    <ListView
-                        dataSource={this.state.dataSource}
-                        renderRow={(rowData) => this._renderRow(rowData)}
-                        renderScrollComponent={props => <RecyclerViewBackedScrollView {...props} />}
-                        renderSeparator={StoryStore._renderSeparator}
-                        renderFooter={this.renderFooter.bind(this)}
-                        onEndReached={this._onRefresh.bind(this, false)}
-                        enableEmptySections={true}
-                        onEndReachedThreshold={10}
-                        scrollEventThrottle={10}
-                        />
-                </View>
-            </View>
+        </Container>
+
         );
 
     }
+    search() {
+        // Set loading to true when the search starts to display a Spinner
+        this.setState({
+            loading: true
+        });
+
+        var that = this;
+        var url = 'http://hardteddy.ru/api/store/search/?keyword='+this.state.search;
+        console.log(url);
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dpbiI6ImFubiIsInR5cGUiOiJ1c2VyIn0.hAxAvPxOJCm73rVwR54MwP7P3SKDmFG0Prsn_JGGzcQ'
+            }})
+            .then((response) => response.json())
+            .then((responseJson) => {
+                // Store the results in the state variable results and set loading to
+                // false to remove the spinner and display the list of repositories
+                that.setState({
+                    results: responseJson,
+                    loading: false
+                });
+                console.log(responseJson)
+                var storiesNew = responseJson.body.stories;
+                this.setState({dataSource: ds.cloneWithRows(storiesNew)})
+
+            })
+            .catch((error) => {
+
+                that.setState({
+                    loading: false
+                });
+
+                console.error(error);
+            });
+    }
+
     changeMode() {
         const newMode = this.state.mode === Picker.MODE_DIALOG
             ? Picker.MODE_DROPDOWN
@@ -199,6 +242,9 @@ export default class StoryStore extends Component{
         //this._onRefresh.(false);
     }
     _onRefresh(isMore) {
+        this.setState({
+            search: ''
+        })
         if (!isMore) {
             this.setState({
                 page: 0,
@@ -228,7 +274,7 @@ export default class StoryStore extends Component{
                 });
                 if(responseJson.status == 0){
                     // Все хорошо
-                    console.log(responseJson)
+                    //console.log(responseJson)
                     //console.log('Status+ '+responseJson.status)
                     var storiesNew = responseJson.body.stories;
                     if (storiesNew.length == 0)  {
@@ -318,6 +364,23 @@ var styles = StyleSheet.create({
     textStyle6: {
         color: '#8e44ad',
         textAlign: 'center'
+    },
+    header : {
+        marginLeft: -5,
+        marginTop: 5,
+        //marginBottom: (Platform.OS==='ios') ? -7 : 0,
+        lineHeight: 24,
+        color: '#5357b6'
+    },
+    modalImage: {
+        resizeMode: 'contain',
+        height: 200
+    },
+    bold: {
+        fontWeight: '600'
+    },
+    negativeMargin: {
+        marginBottom: -10
     }
 });
 
